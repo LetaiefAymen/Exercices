2d2e794b85ecc19ce395a0a25489baf44d7eae8f
Aymen Letaief <letaief.aymen@gmail.com>
2021-12-09 15:50:00 +0100

    initial setup of the project
    setup of the projects with a main module and a core module

---
+++ Exercices/Exercices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercices
+//
+//  Created by Aymen Letaief on 09.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Exercices/ExercicesCore/RemoteExercicesLoader.swift
@@ -0,0 +1,12 @@
+//
+//  RemoteExercicesLoader.swift
+//  ExercicesCore
+//
+//  Created by Aymen Letaief on 09.12.21.
+//
+
+import Foundation
+
+class RemoteExercicesLoader {
+    
+}
